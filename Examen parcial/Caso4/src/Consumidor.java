e1305b340e3ca74478a8827d9ee4663b5b77886a
AnthonyAlc <47694872+AnthonyAlc@users.noreply.github.com>
2020-07-28 18:04:49 -0500

    Create Consumidor.java

---
+++ Examen parcial/Caso4/src/Consumidor.java
@@ -0,0 +1,55 @@
+//package server_4;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class Consumidor {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		try {
+			Socket s = new Socket("3.19.82.156", 2222);
+			
+			
+			DataOutputStream dos = new DataOutputStream(s.getOutputStream());
+			DataInputStream dis = new DataInputStream(s.getInputStream());
+			
+			String id = "0";
+			
+			dos.writeUTF("c:"+id);
+			
+			
+			while(true) {
+				
+				String str = dis.readUTF();
+				
+				String [] partes =  str.split("::");
+				
+				
+				for(int i=0; i<partes.length; i++) {
+					System.out.println("Consumidor ["+id+"] y el mensaje del servidor: "+partes[i]);
+				}
+				
+				
+			}
+			
+			
+			//dos.close();
+			//dis.close();
+			//s.close();
+			
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+}
